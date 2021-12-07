a3c1f34208c66029fc146833b61f0b3c6ae099f9
faizza.mohammed <faizza.mohammed@gmail.com>
2021-12-08 01:09:15 +0530

    changes

---
+++ src/app/Prod.ts
@@ -0,0 +1,8 @@
+interface  Product{
+    "p_name":String,
+    "p_id":number,
+    "p_cost":number,
+    "p_availability":number,
+    "p_details":String,
+    "p_category":String
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  names:any;
   title = 'products';
+  products: any;
+  constructor(private httpClient: HttpClient){}
+  ngOnInit(){
+    this.httpClient.get("assets/data.json").subscribe(data =>{
+      console.log(data);
+      this.products = data;
+      this.names = this.products.p_name;
+      console.log("fhbdfnkjfn",this. names);
+
+    })
+  }
 }
